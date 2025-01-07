6a15243af97094ecf02c3ecdbd1e6d887f3f8ba3
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2025-01-07 20:45:07 -0300

    feat: add PostRepository interface.

---
+++ Src/Repositories/Interfaces/IPostRepository.cs
@@ -0,0 +1,14 @@
+using Catedra3Backend.Src.Models;
+
+namespace Catedra3Backend.Src.Repositories.Interfaces
+{
+    public interface IPostRepository
+    {
+        Task<IEnumerable<Post>> GetPostsAsync();
+        Task<Post?> GetPostByIdAsync(int id);
+        Task<IEnumerable<Post>> GetPostsByUserIdAsync(int userId);
+        Task CreatePostAsync(Post post);
+        Task UpdatePostAsync(Post post);
+        Task DeletePostAsync(int id);
+    }
+}
