95f73be30ac9222f81e9447a6fc2c776da4fe6f3
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2025-01-07 20:39:40 -0300

    feat: add UserRepository Interface.

---
+++ Src/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,14 @@
+using Catedra3Backend.Src.Models;
+
+namespace Catedra3Backend.Src.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetUsersAsync();
+        Task<User?> GetUserByIdAsync(int id);
+        Task<User?> GetUserByEmailAsync(string email);
+        Task CreateUserAsync(User user);
+        Task UpdateUserAsync(User user);
+        Task DeleteUserAsync(string id);
+    }
+}
