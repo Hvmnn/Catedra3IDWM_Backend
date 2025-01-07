c2dc9765d811b55f6dd06b5ec4bb9189c9031924
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2025-01-07 20:32:40 -0300

    fix: edit User model.

--- Src/Models/User.cs
+++ Src/Models/User.cs
@@ -2,7 +2,8 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Catedra3Backend.Src.Models{
 
-    public class User : IdentityUser{
-        public required string Password { get; set; }
+    public class User : IdentityUser
+    {
+
     }
 }
