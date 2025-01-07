bc328903b415d33c092f8d81443f0cd10cf51afb
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2025-01-07 19:49:02 -0300

    fix: implement Identity in User model.

--- Src/Models/User.cs
+++ Src/Models/User.cs
@@ -1,8 +1,8 @@
+using Microsoft.AspNetCore.Identity;
+
 namespace Catedra3Backend.Src.Models{
 
-    public class User {
-        public int Id { get; set; }
-        public required string Email { get; set; } 
+    public class User : IdentityUser{
         public required string Password { get; set; }
     }
 }
