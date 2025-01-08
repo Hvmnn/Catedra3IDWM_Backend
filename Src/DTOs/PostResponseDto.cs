f59733c05e87a417c8847551e3bcbcab4e0f0f1f
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2025-01-07 21:46:19 -0300

    feat: add PostResponseDto.

---
+++ Src/DTOs/PostResponseDto.cs
@@ -0,0 +1,10 @@
+namespace Catedra3Backend.Src.DTOs
+{
+    public class PostResponseDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string PublishedDate { get; set; } = string.Empty;
+        public string ImageUrl { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+    }
+}
